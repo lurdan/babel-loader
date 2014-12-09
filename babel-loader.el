7f71f6fa76721b7697b5ff5871765e8cf3b366df
KURASHIKI Satoru <lurdan@gmail.com>
2014-12-09 14:18:40 +0900

    fix indentation.

--- babel-loader.el
+++ babel-loader.el
@@ -18,33 +18,33 @@
 
 (defun bl:get-file-modified (file)
   (let* ((modified (nth 5 (file-attributes file)))
-		 (high (nth 0 modified))
-		 (low (or (nth 1 modified) 0)))
-	(+ (* high 65536) low)))
+         (high (nth 0 modified))
+         (low (or (nth 1 modified) 0)))
+    (+ (* high 65536) low)))
 
 (defun bl:config-updated-p (file)
   (let* ((base-name (file-name-sans-extension file))
-		 (exported-file (concat base-name ".el")))
+         (exported-file (concat base-name ".el")))
     (if (file-exists-p exported-file)
-		(> (bl:get-file-modified file) (bl:get-file-modified exported-file))
+        (> (bl:get-file-modified file) (bl:get-file-modified exported-file))
       t)))
 
 
 (defun bl:compile (file exported-file)
   (if (bl:config-updated-p file)
-	  (org-babel-tangle-file file exported-file "emacs-lisp")))
+      (org-babel-tangle-file file exported-file "emacs-lisp")))
 
 (defun bl:compile-dir (dir)
   (mapc #'(lambda (file)
-			(let* ((base-name (file-name-sans-extension file))
-				   (exported-file (concat base-name ".el")))
-			  (bl:compile file exported-file)))
-		(directory-files dir t "\\.org$")))
+            (let* ((base-name (file-name-sans-extension file))
+                   (exported-file (concat base-name ".el")))
+              (bl:compile file exported-file)))
+        (directory-files dir t "\\.org$")))
 
 (defun bl:load-dir (dir)
-  (progn 
-	(bl:compile-dir dir)
-	(init-loader-load dir)))
+  (progn
+    (bl:compile-dir dir)
+    (init-loader-load dir)))
 
 (provide 'babel-loader)
 
