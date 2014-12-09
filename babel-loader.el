ba0226ecca04b77ec5f4732fec5dc1337620fd4d
KURASHIKI Satoru <lurdan@gmail.com>
2014-12-09 14:45:48 +0900

    simplify.

--- babel-loader.el
+++ babel-loader.el
@@ -16,22 +16,8 @@
 (require 'init-loader)
 (require 'ob)
 
-(defun bl:get-file-modified (file)
-  (let* ((modified (nth 5 (file-attributes file)))
-         (high (nth 0 modified))
-         (low (or (nth 1 modified) 0)))
-    (+ (* high 65536) low)))
-
-(defun bl:config-updated-p (file)
-  (let* ((base-name (file-name-sans-extension file))
-         (exported-file (concat base-name ".el")))
-    (if (file-exists-p exported-file)
-        (> (bl:get-file-modified file) (bl:get-file-modified exported-file))
-      t)))
-
-
 (defun bl:compile (file exported-file)
-  (if (bl:config-updated-p file)
+  (if (file-newer-than-file-p file exported-file)
       (org-babel-tangle-file file exported-file "emacs-lisp")))
 
 (defun bl:compile-dir (dir)
